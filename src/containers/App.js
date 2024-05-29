8eaff152d43c5797406205fa4d5c4d13e1c90a4d
YunTaeng <howard9166@naver.com>
2024-05-29 15:50:40 +0900

    flask 주현연동

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import MapContainer from './MapContainer';
 import LeftContainer from './LeftContainer';
-import Autocomplete from '../components/autocomplete'; // Autocomplete 컴포넌트의 경로를 올바르게 수정해야 합니다.
-import logo from '../images/Logo.png'; // 로고 이미지의 경로를 올바르게 수정해야 합니다.
+import Autocomplete from '../components/autocomplete';
+import logo from '../images/Logo.png';
 import axios from 'axios';
-import './App.css'; // App.css 파일을 import하여 스타일을 적용합니다.
-import './loading.css';
+import './App.css';
 
 const App = () => {
+    const getCurrentTime = () => {
+        const now = new Date();
+        const kstOffset = 9 * 60 * 60 * 1000; // Offset in milliseconds
+        const kstTime = new Date(now.getTime() + kstOffset);
+        return kstTime.toISOString().substring(11, 16); // Format to HH:mm
+    };
+
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
-    const [departureTime, setDepartureTime] = useState(() => {
-        const now = new Date();
-        return now.toISOString().substring(11, 16); // HH:mm 형식으로 현재 시간을 반환합니다.
-    });
+    const [departureTime, setDepartureTime] = useState(getCurrentTime); // Get current time at initialization
     const [nodeAddr, setNodeAddr] = useState([]);
     const [eta, setEta] = useState('');
     const [path, setPath] = useState([]);
@@ -23,84 +26,55 @@ const App = () => {
     const [map, setMap] = useState(null);
     const [errorMessage, setErrorMessage] = useState('');
     const [showError, setShowError] = useState(false);
-    const [loading, setLoading] = useState(false); // 로딩 상태 변수 추가
     const [mapKey, setMapKey] = useState(0);
 
     const handleSearch = () => {
-        // 입력이 없을경우 
-        if (!origin) setOrigin(null);
-        if (!destination) setDestination(null);
-        if (!departureTime) setDepartureTime(null);
-
         if (origin && destination && departureTime) {
-            setLoading(true); // 로딩 시작
+            console.log('Loading started');
 
-            // Mock response data
-            const mockResponse = {
-                "eta": "15시 41분",
-                "path": [
-                    "대전광역시청",
-                    "유성IC",
-                    "서대전JC",
-                    "계룡IC",
-                    "양촌Hi",
-                    "논산IC",
-                    "논산JC",
-                    "익산IC",
-                    "익산JC",
-                    "삼례IC",
-                    "전주IC",
-                    "서전주IC",
-                    "김제IC",
-                    "금산사IC",
-                    "태인IC",
-                    "정읍IC",
-                    "내장산IC",
-                    "백양사IC",
-                    "장성JC",
-                    "장성IC",
-                    "남장성JC",
-                    "북광산IC",
-                    "광산IC",
-                    "산월IC",
-                    "동림IC",
-                    "서광주IC",
-                    "광주광역시청"
-                ],
-                "total_hours": 3,
-                "total_minutes": 41,
-                "total_seconds": 19
-            }
-            // Use mock response data to set state
-            setEta(mockResponse.eta);
-            setPath(mockResponse.path);
-            setTotalHours(mockResponse.total_hours);
-            setTotalMinutes(mockResponse.total_minutes);
-            setTotalSeconds(mockResponse.total_seconds);
+            const start_point = origin;
+            const end_point = destination;
+            const start_time = departureTime;
+            console.log(start_time);
 
-            const nodeNames = mockResponse.path;
+            axios.post('http://34.47.71.145:5000/find_path', {
+                start_point,
+                end_point,
+                start_time
+            })
+            .then(response => {
+                const { eta, path, total_hours, total_minutes, total_seconds } = response.data;
+                setEta(eta);
+                setPath(path);
+                setTotalHours(total_hours);
+                setTotalMinutes(total_minutes);
+                setTotalSeconds(total_seconds);
 
-            setTimeout(() => {
-                axios.post('http://34.47.71.145:4000/get-node-info', { nodeNames })
-                    .then(response => {
-                        setNodeAddr(response.data);
-                        setMapKey(prevKey => prevKey + 1); // Update mapKey to reload MapContainer
-                    })
-                    .catch(error => {
-                        console.error('Error fetching node info:', error);
-                    })
-                    .finally(() => {
-                        setLoading(false); // 5초 후 로딩 종료
-                    });
-            }, 5000);
+                const nodeNames = path; // path is already an array
+
+                return axios.post('http://34.47.71.145:5000/get-node-info', { nodeNames });
+            })
+            .then(response => {
+                setNodeAddr(response.data);
+                setMapKey(prevKey => prevKey + 1);
+                console.log('Loading stopped');
+            })
+            .catch(error => {
+                console.error('Error fetching route info:', error);
+                console.log('Loading stopped due to error');
+            });
         } else {
-            setErrorMessage('출발지, 목적지를 모두 입력해 주세요');
+            setErrorMessage('전부 입력하시고 검색눌러주세요');
             setShowError(true);
             setTimeout(() => {
                 setShowError(false);
-            }, 3000); // 3초후 에러메세지 삭제
+            }, 3000); // Hide error message after 3 seconds
         }
     };
+
+    useEffect(() => {
+        setDepartureTime(getCurrentTime()); // Set current time when component mounts
+    }, []);
 
     return (
         <div className="App">
@@ -130,20 +104,14 @@ const App = () => {
                     />
                     <button className="SearchButton" onClick={handleSearch}>검색</button>
                     {showError && <div className="ErrorMessage">{errorMessage}</div>}
-                    {loading && (
-                        <div className="loading-container">
-                            <div className="loading"></div>
-                            <div id="loading-text">로딩 중...</div>
-                        </div>
-                    )}
                 </div>
-                <LeftContainer
+                <LeftContainer 
                     nodeAddr={nodeAddr}
-                    map={map}
-                    eta={eta}
-                    totalHours={totalHours}
-                    totalMinutes={totalMinutes}
-                    totalSeconds={totalSeconds}
+                    map={map} 
+                    eta={eta} 
+                    totalHours={totalHours} 
+                    totalMinutes={totalMinutes} 
+                    totalSeconds={totalSeconds} 
                 />
                 <MapContainer key={mapKey} setMap={setMap} nodeAddr={nodeAddr} />
             </div>
